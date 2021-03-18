25654e0d682fe49793b6e4558a0e77270beecfce
Bruno Marques Caroba <brunoxkk0>
2021-03-18 23:32:43 +0000

    Initial commit

---
+++ src/main/java/br/com/brunoxkk0/Application.java
@@ -0,0 +1,10 @@
+package br.com.brunoxkk0;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
